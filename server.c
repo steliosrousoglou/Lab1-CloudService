a8b1fc3b1bd99876ccbe3c8f929707d9326fd305
Stylianos Rousoglou <strousoglou@gmail.com>
2016-09-20 10:08:39 -0400

    condensing code

--- server.c
+++ server.c
@@ -109,6 +109,14 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
     char* endptr;
     char* response;
 
+    const char* arg_id = "node_id";
+    const char* arg_a = "node_a_id";
+    const char* arg_b = "node_b_id";
+
+    struct json_token* find_id = find_json_token(tokens, arg_id);
+    struct json_token* find_a = find_json_token(tokens, arg_a);
+    struct json_token* find_b = find_json_token(tokens, arg_b);
+
     // Sanity check for endpoint length and body not empty
     if(hm->uri.len < 16 || tokens == NULL) {
       badRequest(c);
@@ -116,23 +124,19 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
     }
 
     if(!strncmp(hm->uri.p, "/api/v1/add_node", hm->uri.len)) {
-      const char* arg1 = "node_id";
-
-      struct json_token* find = find_json_token(tokens, arg1);
-
       // body does not contain expected key
-      if(find == 0) {
+      if(find_id == 0) {
         badRequest(c);
         return;
       }
 
       // index of value
-      int index1 = argument_pos(tokens, arg1);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      int index1 = argument_pos(tokens, arg_id);
+      long long arg_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
 
       // returns true if successfully added
-      if(add_vertex(arg1_int)) {
-        response = make_json_one("node_id", 7, arg1_int);
+      if(add_vertex(arg_int)) {
+        response = make_json_one("node_id", 7, arg_int);
         respond(c, "200 OK", strlen(response), response);
         free(response);
       } else {
@@ -141,54 +145,44 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
       }
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/add_edge", hm->uri.len)) {
-      const char* arg1 = "node_a_id";
-      const char* arg2 = "node_b_id";
-
-      struct json_token* find1 = find_json_token(tokens, arg1);
-      struct json_token* find2 = find_json_token(tokens, arg2);
-
-      // body does not contain expected key
-      if(find1 == 0 || find2 == 0) {
+      // body does not contain expected keys
+      if(find_a == 0 || find_b == 0) {
         badRequest(c);
         return;
       }
 
-      // index of value
-      int index1 = argument_pos(tokens, arg1);
-      int index2 = argument_pos(tokens, arg2);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
-      long long arg2_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
+      // index of values
+      int index1 = argument_pos(tokens, arg_a);
+      int index2 = argument_pos(tokens, arg_b);
+      long long arg_a_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      long long arg_b_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
 
       // fix incase of things fucking up
-      switch (add_edge(arg1_int, arg2_int)) {
+      switch (add_edge(arg_a_int, arg_b_int)) {
         case 400:
           respond(c, "400 Bad Request", 0, "");
         case 204:
           respond(c, "204 No Content", 0, "");
         case 200:
-          response = make_json_two("node_a_id", "node_b_id", 9, 9, arg1_int, arg2_int);
+          response = make_json_two("node_a_id", "node_b_id", 9, 9, arg_a_int, arg_b_int);
           respond(c, "200 OK", strlen(response), response);
           free(response);
       }
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/remove_node", hm->uri.len)) {
-      const char* arg1 = "node_id";
-
-      struct json_token* find = find_json_token(tokens, arg1);
-
       // body does not contain expected key
-      if(find == 0) {
+      if(find_id == 0) {
         badRequest(c);
         return;
       }
 
       // index of value
-      int index1 = argument_pos(tokens, arg1);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      int index1 = argument_pos(tokens, arg_id);
+      long long arg_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
 
       // if node does not exist
-      if(remove_vertex(arg1_int)) {
-        response = make_json_one("node_id", 7, arg1_int);
+      if(remove_vertex(arg_int)) {
+        response = make_json_one("node_id", 7, arg_int);
         respond(c, "200 OK", strlen(response), response);
         free(response);
       } else {
@@ -196,27 +190,21 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
       }
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/remove_edge", hm->uri.len)) {
-      const char* arg1 = "node_a_id";
-      const char* arg2 = "node_b_id";
-
-      struct json_token* find1 = find_json_token(tokens, arg1);
-      struct json_token* find2 = find_json_token(tokens, arg2);
-
-      // body does not contain expected key
-      if(find1 == 0 || find2 == 0) {
+      // body does not contain expected keys
+      if(find_a == 0 || find_b == 0) {
         badRequest(c);
         return;
       }
 
-      // index of value
-      int index1 = argument_pos(tokens, arg1);
-      int index2 = argument_pos(tokens, arg2);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
-      long long arg2_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
+      // index of values
+      int index1 = argument_pos(tokens, arg_a);
+      int index2 = argument_pos(tokens, arg_b);
+      long long arg_a_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      long long arg_b_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
 
       // if edge does not exist
-      if(remove_edge(arg1_int, arg2_int)) {
-          response = make_json_two("node_a_id", "node_b_id", 9, 9, arg1_int, arg2_int);
+      if(remove_edge(arg_a_int, arg_b_int)) {
+          response = make_json_two("node_a_id", "node_b_id", 9, 9, arg_a_int, arg_b_int);
           respond(c, "200 OK", strlen(response), response);
           free(response);;
       } else {
@@ -224,75 +212,61 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
       }
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/get_node", hm->uri.len)) {
-      const char* arg1 = "node_id";
-
-      struct json_token* find = find_json_token(tokens, arg1);
-
       // body does not contain expected key
-      if(find == 0) {
+      if(find_id == 0) {
         badRequest(c);
         return;
       }
       // index of value
-      int index1 = argument_pos(tokens, arg1);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      int index1 = argument_pos(tokens, arg_id);
+      long long arg_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
 
-      bool in_graph = get_node(arg1_int);
+      bool in_graph = get_node(arg_int);
       response = make_json_one("in_graph", 8, in_graph);
       respond(c, "200 OK", strlen(response), response);
       free(response);    
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/get_edge", hm->uri.len)) {
-      const char* arg1 = "node_a_id";
-      const char* arg2 = "node_b_id";
-
-      struct json_token* find1 = find_json_token(tokens, arg1);
-      struct json_token* find2 = find_json_token(tokens, arg2);
-
-      // body does not contain expected key
-      if(find1 == 0 || find2 == 0) {
+      // body does not contain expected keys
+      if(find_a == 0 || find_b == 0) {
         badRequest(c);
         return;
       }
 
-      // index of value
-      int index1 = argument_pos(tokens, arg1);
-      int index2 = argument_pos(tokens, arg2);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
-      long long arg2_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
+      // index of values
+      int index1 = argument_pos(tokens, arg_a);
+      int index2 = argument_pos(tokens, arg_b);
+      long long arg_a_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      long long arg_b_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
 
-      if (!get_node(arg1_int) || !get_node(arg2_int)){
+      if (!get_node(arg_a_int) || !get_node(arg_b_int)){
         respond(c, "400 Bad Request", 0, "");
       }
       else {
-        bool in_graph = get_edge(arg1_int, arg2_int);
+        bool in_graph = get_edge(arg_a_int, arg_b_int);
         response = make_json_one("in_graph", 8, in_graph);
         respond(c, "200 OK", strlen(response), response);
         free(response);    
       }
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/get_neighbors", hm->uri.len)) {
-      const char* arg1 = "node_id";
-
-      struct json_token* find1 = find_json_token(tokens, arg1);
-
       // body does not contain expected key
-      if(find1 == 0) {
+      if(find_id == 0) {
         badRequest(c);
         return;
       }
 
       // index of value
-      int index1 = argument_pos(tokens, arg1);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      int index1 = argument_pos(tokens, arg_id);
+      long long arg_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
 
-      if (!get_node(arg1_int) ) {
+      if (!get_node(arg_int) ) {
         respond(c, "400 Bad Request", 0, "");
       } else {
         int size;
-        uint64_t *neighbors = get_neighbors(arg1_int, &size);
+        uint64_t *neighbors = get_neighbors(arg_int, &size);
         char* neighbor_array = format_neighbors(neighbors, size);
-        response = make_neighbor_response("node_id", "neighbors", 7, 9, arg1_int, neighbor_array);
+        response = make_neighbor_response("node_id", "neighbors", 7, 9, arg_int, neighbor_array);
         respond(c, "200 OK", strlen(response), response);
         free(response);
         free(neighbors);
@@ -300,29 +274,23 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
 
     } 
     else if(!strncmp(hm->uri.p, "/api/v1/shortest_path", hm->uri.len)) {
-      const char* arg1 = "node_a_id";
-      const char* arg2 = "node_b_id";
-
-      struct json_token* find1 = find_json_token(tokens, arg1);
-      struct json_token* find2 = find_json_token(tokens, arg2);
-
-      // body does not contain expected key
-      if(find1 == 0 || find2 == 0) {
+      // body does not contain expected keys
+      if(find_a == 0 || find_b == 0) {
         badRequest(c);
         return;
       }
 
-      // index of value
-      int index1 = argument_pos(tokens, arg1);
-      int index2 = argument_pos(tokens, arg2);
-      long long arg1_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
-      long long arg2_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
-
+      // index of values
+      int index1 = argument_pos(tokens, arg_a);
+      int index2 = argument_pos(tokens, arg_b);
+      long long arg_a_int = strtoll(tokens[index1 + 1].ptr, &endptr, 10);
+      long long arg_b_int = strtoll(tokens[index2 + 1].ptr, &endptr, 10);
+      
       // if either node does not exist
-      if(!ret_vertex(arg1_int) || !ret_vertex(arg2_int)) {
+      if(!ret_vertex(arg_a_int) || !ret_vertex(arg_b_int)) {
         respond(c, "400 Bad Request", 0, "");
       } else {
-        int path = shortest_path(arg1_int, arg2_int);
+        int path = shortest_path(arg_a_int, arg_b_int);
         if (path == -1) {
           respond(c, "204 No Content", 0, "");
         } else {
